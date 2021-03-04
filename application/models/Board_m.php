a5364465f504ffea8b9c062bced4e0407cf82808
tech7billie <tech.billie@gmail.com>
2021-03-04 16:44:07 +0900

    게시판 목록 조회 - 쿼리 빌더 클래스 사용

--- application/models/Board_m.php
+++ application/models/Board_m.php
@@ -12,9 +12,16 @@ class Board_m extends CI_Model
  
     function get_list($table = 'ci_board') 
     {
-        // 쿼리문 작성 
-        $sql = "SELECT * FROM ".$table." ORDER BY board_id DESC";
-        $query = $this -> db -> query($sql);
+        // 1. 쿼리문 작성 
+        //$sql = "SELECT * FROM ".$table." ORDER BY board_id DESC";
+        //$query = $this -> db -> query($sql);
+
+        // 2. 쿼리 빌더 클래스 사용 
+        $this->db->order_by('board_id','DESC');
+        $query = $this->db->get($table);
+                        
+
+        
 
         // 쿼리 실행 : 객체 배열 반환 
         $result = $query -> result();           // $result->board_id   형태로 값 얻음 
@@ -24,3 +31,4 @@ class Board_m extends CI_Model
     }
  
 }
+
